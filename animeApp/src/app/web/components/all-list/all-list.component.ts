4160600f839b113a93fc46a958dc1f7027ca8442
jemprojects <jmprojectsgit@gmail.com>
2020-04-26 21:43:16 -0300

    add filter for users

--- animeApp/src/app/web/components/all-list/all-list.component.ts
+++ animeApp/src/app/web/components/all-list/all-list.component.ts
@@ -11,17 +11,19 @@ import { MangasService } from '../../services/mangas.service';
   styleUrls: ['./all-list.component.css']
 })
 export class AllListComponent implements OnInit {
-  tipo:string=''
-  username:string
+  tipo: string = ''
+  userSelect: string = ''
+  username: string
   @HostBinding('class') classes = 'row';
   userLog: string
   animes: Array<Anime>;
   mangas: Array<Manga>
-
-  constructor(private serviceAnime: AnimesService,private serviceManga: MangasService, private authService: AuthService) {
-    this.userLog=this.authService.authStatus.getValue().email
-    this.animes=null
-    this.mangas=null
+  usuarios: string[]
+  list:any
+  constructor(private serviceAnime: AnimesService, private serviceManga: MangasService, private authService: AuthService) {
+    this.userLog = this.authService.authStatus.getValue().email
+    this.animes = null
+    this.mangas = null
   }
 
 
@@ -31,16 +33,33 @@ export class AllListComponent implements OnInit {
       scope.animes = animes;
     });
     this.serviceManga.getMangas(function(mangas) {
-      scope.mangas= mangas
+      scope.mangas = mangas
     })
     console.log(this.animes)
-
-
   }
   loadUsername(cat){
-    this.username= cat.user.split('@')[0].toUpperCase()
-
+    this.username = cat.user.split('@')[0].toUpperCase()
   }
 
+  getUSers(lista){
+    let arregloConRepetidos = lista.map(a => a.user.split('@')[0].toUpperCase())
+    let sinRepetidos = arregloConRepetidos.filter(function(valor, indiceActual, arreglo) {
+      let indiceAlBuscar = arreglo.indexOf(valor);
+      if (indiceActual === indiceAlBuscar) {
+          return true;
+      } else {
+          return false;
+      }
+  });
+    this.usuarios = sinRepetidos
+  }
+  getListForUser(){
+    if (this.tipo == 'mangas'){
+      this.list= this.mangas.filter(m=> m.user.split('@')[0].toUpperCase() === this.userSelect)
+    }
+    else if (this.tipo == 'animes'){
+      this.list= this.animes.filter(m=> m.user.split('@')[0].toUpperCase() === this.userSelect)
+    }
 
+  }
 }
